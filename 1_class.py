225ea82b6cbb2b1d557770d43c2038a922896e81
girishnofficial <girishngirish48@gmail.com>
2026-06-16 17:07:47 +0530

    Added python practice files

---
+++ 1_class.py
@@ -0,0 +1,46 @@
+#variables
+#variable can not give spacess and its a case sensitive
+
+Name ="mrbro" #string
+
+age = 21 #int
+
+tiger_is_a_animal= True #bool
+
+weight =59.78 #float
+
+print(Name)
+print(age)
+print(tiger_is_a_animal)
+print(weight)
+
+
+#we can check the datatype
+print(type(age))
+
+#dynamic changes eg:[bool to string]
+status = True
+print(status)
+
+#change bool to string
+status="tiiger"
+print(status)
+print(type(status))
+
+#type conversion[int to float]
+age=21
+print(age)
+# change int to float
+age=float(age)
+print(age)
+print((type(age)))
+
+
+#Arithmetic_operators
+a=10
+b=15
+print(a+b)
+print(a-b)
+print(a/b)
+print(a*b)
+print(a//b)
